a561e87be9336e04e9dcc6060fa9fa0d3badcc2a
Pranshu Sharma <pranshoe@Pranshus-MacBook-Pro.local>
2025-06-23 21:26:08 +0530

    Moved liquid files from includes to pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("Favicon.png");
     // Copy the admin folder for Decap CMS
     eleventyConfig.addPassthroughCopy("admin");
+    // Copy the entire blog folder (for blog posts and _posts)
+    eleventyConfig.addPassthroughCopy("blog");
+    // Copy the static/uploads folder for Decap CMS images
+    eleventyConfig.addPassthroughCopy("static/uploads");
 
     return {
         dir: {
-            input: ".", // Process files from the current directory
+            input: "pages", // Process files from the current directory
             output: "_site", // Output to a folder named _site
-            includes: "_includes", // Look for layout files in _includes
-            data: "_data" // Look for global data in _data
+            includes: "../_includes", // Look for layout files in _includes
+            data: "../_data" // Look for global data in _data
         },
         templateFormats: ["html", "md", "liquid"], // Process HTML, Markdown, and Liquid templates
         htmlTemplateEngine: "liquid", // Default templating engine for HTML
